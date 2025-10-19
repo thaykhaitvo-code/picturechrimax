2f4ffb24ca03d9b0371ccd60f695c75b5de70378
thaykhaiviet <thaykhaitvo@gmail.com>
2025-10-19 15:17:21 +0700

    Fix JSON syntax for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    base: './',  // ⚡️ THÊM DÒNG NÀY ⚡️
+    base: './',  
     plugins: [react()],
     resolve: {
       alias: {
